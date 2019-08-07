731ada23fa5e9845db2b8baf1cda9ded7dab1225
Jamie Dawson <jadawson@e1z1r7p31.42.us.org>
2019-08-07 16:26:33 -0700

    now takes in start dates

--- playground.esports/code/my_search.js
+++ playground.esports/code/my_search.js
@@ -5,27 +5,27 @@ module.exports.function = function my_search (games) {
   var stuff = "not found";
   var template;
   var results = [];
-  var apiURL = "https://api.pandascore.co/leagues?token=4x5vC1weSK1XFRkBtMdt_m5LkZxit0PeQ2hvu5Go-63IRH5amj8";
-  
+  var apiURL = "https://api.pandascore.co/leagues?token=4x5vC1weSK1XFRkBtMdt_m5LkZxit0PeQ2hvu5Go-63IRH5amj8"; 
   var tmpResults = http.getUrl(apiURL, {format: 'text'});
   tmpResults = JSON.parse(tmpResults) 
-  console.log(tmpResults) 
-  console.log("FIRST!")
+  var j = 1;
   
   if (games == "overwatch")
     console.log("found overwatch")
   
   for (var i = 0; i < tmpResults.length; i++) {
-    var date = tmpResults[i].series
+    if (j == 5)
+      break
+    var date = tmpResults[i].series[0]
     template = {
        leauge_name: tmpResults[i].name,
-      image: {
+      image_URL: {
         url: tmpResults[i].image_url 
       },
       start_date: date.begin_at
     }
     results.push(template)
+    j++;
   }
-  
   return results;
 }
