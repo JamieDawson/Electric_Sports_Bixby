df2b203910aaefa931966c960aae6014ee1c8e23
Jamie Dawson <jadawson@e1z3r4p15.42.us.org>
2019-08-11 19:30:38 -0700

    still need to fix array issue with teams. But I am at least pulling one team image

--- playground.esports/code/my_search.js
+++ playground.esports/code/my_search.js
@@ -8,14 +8,14 @@ module.exports.function = function my_search (games) {
   var apiURL = "https://api.pandascore.co/tournaments?token=4x5vC1weSK1XFRkBtMdt_m5LkZxit0PeQ2hvu5Go-63IRH5amj8"; 
   var tmpResults = http.getUrl(apiURL, {format: 'text'});
   tmpResults = JSON.parse(tmpResults) 
-
+  var teamNum = 0;
+  
+  
 if (games == "league of legends" || games == "League of Legends") //checks if human says dota 2
   {
     for (var i = 0; i < tmpResults.length; i++) {  
       if (tmpResults[i].videogame.name == "LoL"){ //checks json has "Dota 2"
         var date = tmpResults[i]
-      
-      
   
         template = {
           leauge_name: tmpResults[i].league.slug,
@@ -23,12 +23,12 @@ if (games == "league of legends" || games == "League of Legends") //checks if hu
           url: tmpResults[i].league.image_url 
           },
            start_date: date.begin_at,
-          
           team_image_URL: {
-          url: tmpResults[i].teams[0].image_url
+          url: tmpResults[i].teams[teamNum].image_url
          }      
        }
-        console.log(tmpResults[i].teams[0].image_url)
+  
+       console.log(tmpResults[i].teams[0].image_url)
      }  
         results.push(template)
       } 
