8640da372ae979fd6129d91847d86acef4269740
Jamie Dawson <jadawson@e1z3r4p15.42.us.org>
2019-08-11 17:43:46 -0700

    changing to tournaments search. My json returns the compitition photos. Need to grab more info

--- playground.esports/code/my_search.js
+++ playground.esports/code/my_search.js
@@ -5,150 +5,28 @@ module.exports.function = function my_search (games) {
   var stuff = "not found";
   var template;
   var results = [];
-  var apiURL = "https://api.pandascore.co/leagues?token=4x5vC1weSK1XFRkBtMdt_m5LkZxit0PeQ2hvu5Go-63IRH5amj8"; 
+  var apiURL = "https://api.pandascore.co/tournaments?token=4x5vC1weSK1XFRkBtMdt_m5LkZxit0PeQ2hvu5Go-63IRH5amj8"; 
   var tmpResults = http.getUrl(apiURL, {format: 'text'});
   tmpResults = JSON.parse(tmpResults) 
-  var j = 1;
-  
-  
-  //If user request Overwatch Leagues!
-  if (games == "Overwatch" || games == "overwatch")
+
+if (games == "league of legends" || games == "League of Legends") //checks if human says dota 2
   {
-    for (var i = 0; i < tmpResults.length; i++) {
-      if (j == 40)
-        break
-      if (tmpResults[i].videogame.name == "Overwatch"){   
-      var date = tmpResults[i].series[0]
-      
-      if(tmpResults[i].series[0].hasOwnProperty('begin_at')) {
-      template = {
-         leauge_name: tmpResults[i].name,
-        image_URL: {
-          url: tmpResults[i].image_url 
-        },
-        start_date: date.begin_at
-       }
-      }
-        
-      if(typeof(tmpResults[i].series[0]) !== "undefined")     
-       results.push(template)
-      j++;
-      }
-    }
-  }
-  
-  //If user request Dota 2
-  else if (games == "dota 2" || games == "dota two") //checks if human says dota 2
-  {
-    for (var i = 0; i < tmpResults.length; i++) {
-      if (j == 60)
-        break
-      if (tmpResults[i].videogame.name == "Dota 2"){ //checks json has "Dota 2"
-      var date = tmpResults[i].series[0]
-      
-    if(typeof(tmpResults[i].series[0]) !== "undefined") { //Checks if it has a series object.
-      template = {
-         leauge_name: tmpResults[i].name,
-        image_URL: {
-          url: tmpResults[i].image_url 
-        },
-          start_date: date.begin_at
-      }
-     }
-        
-      if(typeof(tmpResults[i].series[0]) !== "undefined")
-        results.push(template)
-      j++;
-      }
-    }
-  }
-  
- 
-  //If user request counter strike
-  else if (games == "counter strike" || games == "Counter Strike") //checks if human says dota 2
-  {
-    //console.log("COUNTER STRIKE HERE!!!")
-    for (var i = 0; i < tmpResults.length; i++) {
-      if (j == 60)
-        break
-      if (tmpResults[i].videogame.name == "CS:GO"){ //checks json has "Dota 2"
-      var date = tmpResults[i].series[0]
-      
-    if(typeof(tmpResults[i].series[0]) !== "undefined") { //Checks if it has a series object.
-      template = {
-         leauge_name: tmpResults[i].name,
-        image_URL: {
-          url: tmpResults[i].image_url 
-        },
-          start_date: date.begin_at
-      }
-     }
-        
-      if(typeof(tmpResults[i].series[0]) !== "undefined")
-        results.push(template)
-      j++;
-      }
-    }
-  }
-  
-  
-  
-   //If user request counter strike
-  else if (games == "league of legends" || games == "League of Legends") //checks if human says dota 2
-  {
-    console.log("LEAGUE OF LEGENDS!!!")
-    for (var i = 0; i < tmpResults.length; i++) {
-      if (j == 60)
-        break
+    for (var i = 0; i < tmpResults.length; i++) {  
       if (tmpResults[i].videogame.name == "LoL"){ //checks json has "Dota 2"
-      var date = tmpResults[i].series[0]
+       // var date = tmpResults[i].series[0]
       
-    if(typeof(tmpResults[i].series[0]) !== "undefined") { //Checks if it has a series object.
-      template = {
-         leauge_name: tmpResults[i].name,
-        image_URL: {
-          url: tmpResults[i].image_url 
-        },
-          start_date: date.begin_at
-      }
-     }
-        
-      if(typeof(tmpResults[i].series[0]) !== "undefined")
-        results.push(template)
-      j++;
-      }
-    }
-  }
-  
-  
-  
-  
-  
-  
-  
-  //if no video game is given!
-  else {
-        for (var i = 0; i < tmpResults.length; i++) {
-      if (j == 200)
-        break 
-      var date = tmpResults[i].series[0]
       
-       if(typeof(tmpResults[i].series[0]) !== "undefined"){
-          template = {
-           leauge_name: tmpResults[i].name,
-           image_URL: {
-           url: tmpResults[i].image_url 
-        },
-      start_date: date.begin_at
-     } 
-    }
-          
-      if(typeof(tmpResults[i].series[0]) !== "undefined")
+  
+        template = {
+          leauge_name: tmpResults[i].name,
+          image_URL: {
+          url: tmpResults[i].league.image_url 
+          },
+       //    start_date: date.begin_at
+        }
+     }  
         results.push(template)
-      j++;
+      } 
     }
-  }
-  
-  
   return results;
 }
