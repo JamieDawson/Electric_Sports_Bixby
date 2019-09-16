2a90408bf41347a214519d09f3a9a80ffc735e4d
Jamie Dawson <jadawson@e1z2r7p48.42.us.org>
2019-09-15 21:30:32 -0700

    fixed details

--- playground.esports/code/my_search.js
+++ playground.esports/code/my_search.js
@@ -77,35 +77,35 @@ module.exports.function = function my_search(games) {
       }
     }
   }
+  else if (games == "counter strike" || games == "Counter Strike") {
+    for (var i = 0; i < tmpResults.length; i++) {
+      var template = {}
+      if (tmpResults[i].videogame.name == "CS:GO") { //checks json has "Dota 2"
+        var date = tmpResults[i]
+        var teamImages = []; //all team images.
+        for (var j = 0; j < tmpResults[i].teams.length; j++)
+          teamImages.push({
+            url: tmpResults[i].teams[j].image_url,
+            caption: tmpResults[i].teams[j].name
+          });
 
-  //   else if (games == "counter strike" || games == "Counter Strike") { 
-  //     for (var i = 0; i < tmpResults.length; i++) {  
-  //       if (tmpResults[i].videogame.name == "CS:GO") { //checks json has "Dota 2"
-  //         console.log("DO I GET HERE!!!")
-  //         var date = tmpResults[i]
-  //         var teamImages = []; //all team images.
-  //         for (var j = 0; j < tmpResults[i].teams.length; j++)
-  //           teamImages.push({
-  //             url: tmpResults[i].teams[j].image_url,
-  //             caption: tmpResults[i].teams[j].name
-  //           });
-  //         
-  //         var youtube_search = "https://www.youtube.com/results?search_query=" + tmpResults[i].league.slug;
-  //         var the_name = "counter strike"       
-  //         
-  //         template = {
-  //           leauge_name: tmpResults[i].league.slug,
-  //           image_URL: {
-  //             url: tmpResults[i].league.image_url 
-  //           },
-  //           start_date: date.begin_at,
-  //           team_image_URL: teamImages,
-  //           youtube_link: youtube_search,
-  //           name_of_the_game: the_name,
-  //         }
-  //       }  
-  //       results.push(template)
-  //     } 
-  //   }
+        var youtube_search = "https://www.youtube.com/results?search_query=" + tmpResults[i].league.slug;
+        var the_name = "counter strike"
+
+        template = {
+          leauge_name: tmpResults[i].league.slug.replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " ").replace("-", " "),
+          image_URL: {
+            url: tmpResults[i].league.image_url
+          },
+          start_date: tmpResults[i].begin_at.substring(0, 10).replace("-", "/").replace("-", "/"),
+          end_date: tmpResults[i].end_at ? tmpResults[i].end_at.substring(0, 10).replace("-", "/").replace("-", "/") : 'TBA',
+          team_image_URL: teamImages,
+          youtube_link: youtube_search,
+          name_of_the_game: the_name,
+        }
+      }
+      results.push(template)
+    }
+  }
   return results;
 }
